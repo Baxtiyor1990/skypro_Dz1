133096c586f046886b234dd56225bf3ff6941cd3
Baxtiyor1990 <156651092+Baxtiyor1990@users.noreply.github.com>
2024-03-07 11:29:18 +0500

    домашка

--- modules/category.py
+++ modules/category.py
@@ -1,13 +1,21 @@
 
+from .exceptions import ZeroQuantityException
+
 class Category:
     def __init__(self, name: str) -> None:
         self.name = name
-        self.products = []
+        self.__products = []
 
-    def calculate_average_price(self):
-        try:
-            total_price = sum(product.price for product in self.products)
-            average_price = total_price / len(self.products)
-        except ZeroDivisionError:
+    def add_product(self, product) -> None:
+        if product.quantity == 0:
+            raise ValueError("Нельзя добавить товар с нулевым количеством!")
+        self.__products.append(product)
+
+    def average_price(self) -> float:
+        total_price = sum([product.price for product in self.__products])
+        total_quantity = sum([product.quantity for product in self.__products])
+
+        if total_quantity == 0:
             return 0
-        return average_price
+
+        return total_price / total_quantity
